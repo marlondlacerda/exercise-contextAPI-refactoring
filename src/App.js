f8484de74ef76e539d6357a506b143cb68651957
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-09-14 19:27:08 -0300

    Exercise one done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 import Cars from './Cars';
 
+
+
+
 function App() {
   return (
-    <Cars />
+      <Cars />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux';
+import Provider from './context/Providers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <App />
-    </Provider>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
